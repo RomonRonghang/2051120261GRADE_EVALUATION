cb9e49fbe085a59cf5bc627193be5ea4d92f4a43
RomonRonghang <205112061@nitt.edu>
2013-10-15 16:27:32 +0530

    GRADE EVALUATION

---
+++ GPA/GPA/GPA/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GPA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int d;
+
+            for (d = 0; d <= 100; d++)
+                progressBar1.Value = d;
+
+            this.Visible = false;
+            Form2 g = new Form2();
+            g.Show();
+
+            timer1.Enabled = false;
+        }
+    }
+}
---
+++ GPA/GPA/GPA/Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace GPA
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string constr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=G:\GPA\GPA\\GPA.mdb;";
+            Con = new OleDbConnection(@constr);
+            Con.Open();
+
+            Com = new OleDbCommand();
+            Com.
+        }
+    }
+}
